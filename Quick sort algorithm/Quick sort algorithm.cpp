b38900757319d1bb129e6f28b7496d19299e39d1
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-10-06 11:22:29 -0700

    Commits have been added

--- Quick sort algorithm/Quick sort algorithm.cpp
+++ Quick sort algorithm/Quick sort algorithm.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
+//Pivot will be first element:
 int Partition(int arr[], int start, int end)
 {
     int Pivot = arr[start];
     int i = start;
     int j = end;
 
+    //loop will run untill i and j crossed
     while (i < j)
     {
+        // mov i pointer rightword if ALL IS WELL
         while (arr[i] <= Pivot && i <= end - 1)
         {
             i++;
         }
+        // mov j pointer leftward if ALL IS WELL
         while (arr[j] > Pivot && j >= start + 1)
         {
             j--;
         }
+        //we have two elements that need to swap with each other but their pointers did't crossed:
         if (i < j)
         {
             int temp = arr[i];
@@ -24,11 +29,11 @@ int Partition(int arr[], int start, int end)
             arr[j] = temp;
         }
     }
-    
+    //Pointers have been crossed then shuffle Pivot value with jth index
     int temp = arr[start];
     arr[start] = arr[j];
     arr[j] = temp;
-
+    //return index from which we replaced Pivot
     return j;
 }
 
